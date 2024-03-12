db0ed9bb8c527e3e2b0ba3fc5503e973f94728f7
Simon <97980774+SimonReis@users.noreply.github.com>
2024-03-12 13:48:55 +0100

    with image, not a good solution

--- src/panels/InformationPanelLeft.java
+++ src/panels/InformationPanelLeft.java
@@ -5,6 +5,7 @@ import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -15,13 +16,18 @@ public class InformationPanelLeft extends JPanel {
 	private static final long serialVersionUID = 1L;
 	
 	public InformationPanelLeft(Color color) {
+		
 		this.setLayout(new BorderLayout());
 		this.setBackground(color);
-
-		JLabel titleLabel = new JLabel("Tetris");
-		titleLabel.setFont(new Font("Arial", Font.BOLD, 65));
-        titleLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
-        this.add(titleLabel, BorderLayout.NORTH);
+		
+		ImageIcon image = new ImageIcon("/Users/simonreis/Downloads/aaa_tetris_logo.jpg");
+		
+		JLabel logo = new JLabel(image);
+//		JLabel titleLabel = new JLabel("Tetris");
+//		titleLabel.setFont(new Font("Arial", Font.BOLD, 65));
+//        titleLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        
+        this.add(logo, BorderLayout.NORTH);
         
         JButton pauseButton = new JButton("Pause Game");
         this.add(pauseButton, BorderLayout.SOUTH);
