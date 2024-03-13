6902193ae3d61b45019e23d5a82bdc9df854b826
ynrmd <yoann.normand@outlook.com>
2024-03-13 15:28:30 +0100

    block going down and stopping at the bottom

--- src/tetris/Block.java
+++ src/tetris/Block.java
@@ -1,11 +1,51 @@
 package tetris;
 
-public class Block {
+import java.util.TimerTask;
+
+public class Block extends TimerTask {
 	
-	public Block() {
+	private GridTable table;
+	private int row;
+	private int col;
+	
+	public Block(GridTable table, int row, int col) {
+		this.table = table;
+		this.row = row;
+		this.col = col;
+		table.setValueAt(1, row, col);
+	}
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		System.out.println("Action performed at: " + new java.util.Date());
+		if (row < 19) {
+			table.setValueAt(0, row, col);
+			row++;
+			table.setValueAt(1, row, col);
+		} else {
+			System.out.println("Action exit");
+			cancel();
+		}
 		
-		
-		
+	}
+	
+	
+	
+	public int getRow() {
+		return row;
+	}
+	
+	public int getCol() {
+		return col;
+	}
+	
+	public void setRow(int row) {
+		this.row = row;
+	}
+	
+	public void setCol(int col) {
+		this.col = col;
 	}
 
 }
--- src/tetris/Tetris.java
+++ src/tetris/Tetris.java
@@ -24,8 +24,9 @@ public class Tetris {
 		frame.setVisible(true);
 		frame.pack();
         Timer timer = new Timer();
-        long delay = 1000; // Delay in milliseconds
-        timer.scheduleAtFixedRate(new GameEngine(table), 0, delay);
+        long delay = 100; // Delay in milliseconds
+        timer.scheduleAtFixedRate(new Block(table, 0, 5), 0, delay);
+        
 	}
 
 }
