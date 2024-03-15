5524e586b27a007bbf01805bcbdb15cb9a7c8ea9
Simon <97980774+SimonReis@users.noreply.github.com>
2024-03-15 11:10:56 +0100

    nextGrid is included, optimise layout

--- src/panels/InformationPanelRight.java
+++ src/panels/InformationPanelRight.java
@@ -10,6 +10,8 @@ import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import tetris.Grid;
+
 //TODO This can be simplified!!! 
 
 /**
@@ -23,8 +25,8 @@ public class InformationPanelRight extends JPanel {
 	/**
 	 * All components to build the panel
 	 */
-	private JPanel highScorePanel, scorePanel, linesPanel, nextTetrominoPanel;
-	private JLabel highScoreLabelText, highScoreLabelNumber, scoreLabelText, scoreLabelNumber, linesPanelText, linesPanelNumber;
+	private JLabel highScoreLabelNumber, scoreLabelNumber, linesLabelNumber;
+	private Grid nextGrid;
 	private Font font;
 	private Color color;
 
@@ -37,11 +39,11 @@ public class InformationPanelRight extends JPanel {
 
 		this.color = color;
 		initPanel();
-		initHighScore();
-		initScore();
-		initLines();
-		initNextTetromino();
-
+		highScoreLabelNumber = createInformation("Lines");
+		scoreLabelNumber = createInformation("Score");
+		linesLabelNumber = createInformation("Lines");
+		nextGrid = createNextGrid("Next");
+		
 	}
 
 	/**
@@ -54,129 +56,52 @@ public class InformationPanelRight extends JPanel {
 		font = new Font("Arial", Font.BOLD, 20);
 	}
 
-	/**
-	 * This method initializes the panel where the information for the current high
-	 * score are stored.
-	 */
-	private void initHighScore() {
 
-		highScore = 0;
-
-		//Create panel where text and value can be stored underneath
-		highScorePanel = new JPanel();
-		highScorePanel.setLayout(new GridLayout(2, 1));
-		highScorePanel.setBackground(color);
+	private JLabel createInformation(String name) {
+		JPanel panel = new JPanel();
+		panel.setLayout(new GridLayout(2, 1));
+		panel.setBackground(color);
 		
-		highScoreLabelText = new JLabel("Highscore:");
-		highScoreLabelText.setLayout(null);
-		highScoreLabelText.setFont(font);
-		highScoreLabelText.setHorizontalAlignment(JLabel.CENTER);
-		highScoreLabelText.setVerticalAlignment(JLabel.BOTTOM);
-		highScorePanel.add(highScoreLabelText);
+		JLabel labelText = new JLabel(name + ":");
+		labelText.setLayout(null);
+		labelText.setFont(font);
+		labelText.setHorizontalAlignment(JLabel.CENTER);
+		labelText.setVerticalAlignment(JLabel.BOTTOM);
+		panel.add(labelText);
 
-		highScoreLabelNumber = new JLabel(Integer.toString(highScore));
-		highScoreLabelNumber.setLayout(null);
-		highScoreLabelNumber.setFont(font);
-		highScoreLabelNumber.setHorizontalAlignment(JLabel.CENTER);
-		highScoreLabelNumber.setVerticalAlignment(JLabel.TOP);
-		highScorePanel.add(highScoreLabelNumber);
-
-		this.add(highScorePanel);
-
+		JLabel labelNumber = new JLabel(Integer.toString(0));
+		labelNumber.setLayout(null);
+		labelNumber.setFont(font);
+		labelNumber.setHorizontalAlignment(JLabel.CENTER);
+		labelNumber.setVerticalAlignment(JLabel.TOP);
+		panel.add(labelNumber);
+		this.add(panel);
+		
+		return labelNumber;
 	}
 	
-	/**
-	 * This method initializes the panel where the information for the current high
-	 * score are stored.
-	 */
-	private void initScore() {
-
-		score = 0;
-
-		//Create panel where text and value can be stored underneath
-		scorePanel = new JPanel();
-		scorePanel.setLayout(new GridLayout(2, 1));
-		scorePanel.setBackground(color);
+	private Grid createNextGrid(String name) {
+		JPanel panel = new JPanel();
+		panel.setLayout(new GridLayout(2, 1));
+		panel.setBackground(color);
 		
-		scoreLabelText = new JLabel("Score:");
-		scoreLabelText.setLayout(null);
-		scoreLabelText.setFont(font);
-		scoreLabelText.setHorizontalAlignment(JLabel.CENTER);
-		scoreLabelText.setVerticalAlignment(JLabel.BOTTOM);
-		scorePanel.add(scoreLabelText);
+		JLabel labelText = new JLabel(name + ":");
+		labelText.setLayout(null);
+		labelText.setFont(font);
+		labelText.setHorizontalAlignment(JLabel.CENTER);
+		labelText.setVerticalAlignment(JLabel.BOTTOM);
+		panel.add(labelText);
 
-		scoreLabelNumber = new JLabel(Integer.toString(score));
-		scoreLabelNumber.setLayout(null);
-		scoreLabelNumber.setFont(font);
-		scoreLabelNumber.setHorizontalAlignment(JLabel.CENTER);
-		scoreLabelNumber.setVerticalAlignment(JLabel.TOP);
-		scorePanel.add(scoreLabelNumber);
-
-		this.add(scorePanel);
-
-	}
-	
-	/**
-	 * This method initializes the panel where the information for the current high
-	 * score are stored.
-	 */
-	private void initLines() {
-
-		clearedLines = 0;
-
-		//Create panel where text and value can be stored underneath
-		linesPanel = new JPanel();
-		linesPanel.setLayout(new GridLayout(2, 1));
-		linesPanel.setBackground(color);
+		JPanel nextPanel = new JPanel();
+		nextPanel.setBackground(color);
+		//nextPanel.setLayout(null);
+		Grid nextGrid = new Grid(4,4);
+		nextPanel.add(nextGrid);
+		panel.add(nextPanel);
+		this.add(panel);
 		
-		linesPanelText = new JLabel("Lines:");
-		linesPanelText.setLayout(null);
-		linesPanelText.setFont(font);
-		linesPanelText.setHorizontalAlignment(JLabel.CENTER);
-		linesPanelText.setVerticalAlignment(JLabel.BOTTOM);
-		linesPanel.add(linesPanelText);
-
-		linesPanelNumber = new JLabel(Integer.toString(clearedLines));
-		linesPanelNumber.setLayout(null);
-		linesPanelNumber.setFont(font);
-		linesPanelNumber.setHorizontalAlignment(JLabel.CENTER);
-		linesPanelNumber.setVerticalAlignment(JLabel.TOP);
-		linesPanel.add(linesPanelNumber);
-
-		this.add(linesPanel);
-
+		return nextGrid;
 	}
-	
-	
-	/**
-	 * This method initializes the panel where the information for the current high
-	 * score are stored.
-	 */
-	private void initNextTetromino() {
 
-		//Grid
-
-		//Create panel where text and value can be stored underneath
-		linesPanel = new JPanel();
-		linesPanel.setLayout(new GridLayout(2, 1));
-		linesPanel.setBackground(color);
-		
-		linesPanelText = new JLabel("Next:");
-		linesPanelText.setLayout(null);
-		linesPanelText.setFont(font);
-		linesPanelText.setHorizontalAlignment(JLabel.CENTER);
-		linesPanelText.setVerticalAlignment(JLabel.BOTTOM);
-		linesPanel.add(linesPanelText);
-
-		linesPanelNumber = new JLabel("Insert 4x4 Grid here");
-		linesPanelNumber.setLayout(null);
-		linesPanelNumber.setFont(font);
-		linesPanelNumber.setHorizontalAlignment(JLabel.CENTER);
-		linesPanelNumber.setVerticalAlignment(JLabel.TOP);
-		linesPanel.add(linesPanelNumber);
-
-		this.add(linesPanel);
-
-	}
 
 }
