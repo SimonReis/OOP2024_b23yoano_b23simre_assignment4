0d8c6151fc2a9f45e143091d9a61e468c45762a9
Simon <97980774+SimonReis@users.noreply.github.com>
2024-03-21 15:02:22 +0100

    grid check

--- src/tetris/Grid.java
+++ src/tetris/Grid.java
@@ -7,19 +7,13 @@ import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableColumn;
 
-
 /**
  * This class creates a grid with cells in form of a square.
  */
 public class Grid extends JTable {
-	
+
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * Properties of the grid
-	 */
-	private int numRows, numCols, cellSize;
-	
 	/**
 	 * This constructor create a grid by a given number of rows and columns.
 	 * 
@@ -27,64 +21,25 @@ public class Grid extends JTable {
 	 * @param numCols Number of columns
 	 */
 	public Grid(int numRows, int numCols) {
+
 		DefaultTableModel model = new DefaultTableModel(numRows, numCols) {
 
 			private static final long serialVersionUID = 1L;
 
 			@Override
-		    public boolean isCellEditable(int row, int column) {
-		       //all cells false
-		       return false;
-		    }
+			public boolean isCellEditable(int row, int column) {
+				// all cells false
+				return false;
+			}
 		};
-		
+
 		this.setModel(model);
 		this.setGridColor(Color.LIGHT_GRAY);
-		this.setRowHeight(24); //TODO Maybe add resize later
+		this.setRowHeight(24); // TODO Maybe add resize later
 		for (int i = 0; i < this.getColumnModel().getColumnCount(); i++) {
 			TableColumn column = this.getColumnModel().getColumn(i);
 			column.setPreferredWidth(24);
 		}
 		this.setDefaultRenderer(Object.class, new GridRenderer());
 	}
-	
-	/**
-	 * This method sets the size of the table by a given cell size. Whereas one cell is a square.
-	 * 
-	 * @param cellSize Number of pixels for the cell height and width
-	 */
-	private void setTableSize(int cellSize) {
-		this.cellSize = cellSize;
-		Dimension prefferedSize = new Dimension(this.cellSize * numCols, this.cellSize * numRows);
-		this.setPreferredSize(prefferedSize);
-		this.setRowHeight(this.cellSize); 
-		
-		for (int i = 0; i < this.getColumnModel().getColumnCount(); i++) {
-			TableColumn column = this.getColumnModel().getColumn(i);
-			column.setPreferredWidth(this.cellSize);
-		}
-		
-	}
-	
-
-	
-/**
- * Not needed yet.
- */
-//	public int getCellSize() {
-//		return cellSize;
-//	}
-//
-//	public void setCellSize(int cellSize) {
-//		this.cellSize = cellSize;
-//		setTableSize(this.cellSize);
-//		
-//	}
-		
-/**
- * Not needed yet.
- */	
-//	public int getTableHeight() {
-//		return numRows*cellSize;
-//	}
 }
